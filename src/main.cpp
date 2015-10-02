9aa1293d89fb8387c2f808af98f1ebc78af88520
lyx-x <li.yuxiang.nj@gmail.com>
2015-10-02 22:08:56 +0200

    Use VTK

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include "opencv2/opencv.hpp"
+#include "vtkRenderWindow.h"
+#include "vtkSmartPointer.h"
 
 #include "geometry/triangle.h"
 #include "geometry/line.h"
@@ -12,11 +14,18 @@ using namespace std;
 using namespace sketch;
 
 int main() {
+    vtkSmartPointer <vtkRenderWindow> renWin = vtkSmartPointer<vtkRenderWindow>::New();
+
+    renWin->Render();
+
+
     cout << "Hello, World!" << endl;
+    /*
     Triangle tri(Point3f(1.23f, 1, 0.5), Point3f(2, 2, 1), Point3f(3, 4, -3));
     Line line(Point3f(1, 1, 1), Point3f(2.26, 5, 0.4));
     ifstream input("../models/models/m0.off");
     Model model(input);
     cout << tri << line << model;
+    */
     return 0;
 }
