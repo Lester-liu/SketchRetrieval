4d449599f05426d83368451bab70cd72db881cd7
lyx-x <li.yuxiang.nj@gmail.com>
2015-11-11 16:10:50 +0100

    PLY Reader

--- utils/Renderer/renderer.cpp
+++ utils/Renderer/renderer.cpp
@@ -1,8 +1,47 @@
-#include <iostream>
+#include <vtkPolyData.h>
+#include <vtkPLYReader.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
 
-using namespace std;
+int main(int argc, char *argv[]) {
+    std::string inputFilename;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+    if(argc != 2)
+        inputFilename = "/home/lyx/workspace/cuda/Sketch/models/models_ply/m0.ply";
+    else
+        inputFilename = argv[1];
+
+    vtkSmartPointer<vtkPLYReader> reader =
+            vtkSmartPointer<vtkPLYReader>::New();
+    reader->SetFileName ( inputFilename.c_str() );
+
+    // Visualize
+    vtkSmartPointer<vtkPolyDataMapper> mapper =
+            vtkSmartPointer<vtkPolyDataMapper>::New();
+    mapper->SetInputConnection(reader->GetOutputPort());
+
+    vtkSmartPointer<vtkActor> actor =
+            vtkSmartPointer<vtkActor>::New();
+    actor->SetMapper(mapper);
+
+    vtkSmartPointer<vtkRenderer> renderer =
+            vtkSmartPointer<vtkRenderer>::New();
+    vtkSmartPointer<vtkRenderWindow> renderWindow =
+            vtkSmartPointer<vtkRenderWindow>::New();
+    renderWindow->AddRenderer(renderer);
+    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+            vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    renderWindowInteractor->SetRenderWindow(renderWindow);
+
+    renderer->AddActor(actor);
+    renderer->SetBackground(0.1804,0.5451,0.3412); // Sea green
+
+    renderWindow->Render();
+    renderWindowInteractor->Start();
+
+    return EXIT_SUCCESS;
 }
