14c8ae7caf55803efb75e20672b4fe1655caa778
lyx-x <li.yuxiang.nj@gmail.com>
2015-11-11 21:44:31 +0100

    Read arguments

--- utils/Renderer/renderer.cpp
+++ utils/Renderer/renderer.cpp
@@ -37,24 +37,25 @@
 #include <vtkWindowToImageFilter.h>
 #include <vtkPNGWriter.h>
 
+#include <iostream>
 #include <cmath>
-
-typedef double* Color;
 
 using namespace std;
 
+typedef double* Color; // 3 double numbers
+enum Mode {Single, Group};
+
 struct Cartesian {
     double x, y, z;
-    Cartesian(double x, double y, double z): x(x), y(y), z(z) {}
-    Cartesian(){x = 0; y = 0; z = 0;};
-};
 
-bool single_mode = false, group_mode = false;
-Cartesian position;
-Color background_color;
-char *output_file_name = NULL, *model_file_name = NULL;
-double camera_distance = 2.4;
-int number_of_images = 1;
+    Cartesian(double x, double y, double z): x(x), y(y), z(z) {}
+
+    Cartesian() {
+        x = 0;
+        y = 0;
+        z = 0;
+    };
+};
 
 Cartesian from_polar(double r, double phi, double theta) {
     double x, y, z;
@@ -64,7 +65,20 @@ Cartesian from_polar(double r, double phi, double theta) {
     return Cartesian(x, y, z);
 }
 
-void exit_with_help(){
+string output, input; // model and image file name
+
+Mode mode = Single;
+Color background; // model background color
+
+Cartesian position; // camera position
+double camera_distance = 2.4; // initial distance form the mass center of the object
+double phi, theta; // camera angles
+
+int step_count = 1;
+double min_angle = 0;
+double max_angle = 360;
+
+void show_help(){
     printf("3D Mesh (PLY) rendering\n"
                    "Usage 1 - single image:\n"
                    "Parameters:\n"
@@ -83,123 +97,124 @@ void exit_with_help(){
                    "      f (path to PLY model)\n"
                    "      t (path of the folder where images will be generated)\n"
                    " ex: Renderer -g [-p r] [-c r g b] [-n Number] -f Path_to_model_file -t Path_to_image_folder);\n");
-    exit(1);
 }
 
-void parse_command_line(int argc, char **argv){
+/*
+ * Process all arguments
+ */
+bool parse_command_line(int argc, char **argv) {
+
     int i = 1;
-    while(i < argc){
+    while(i < argc) {
         if (argv[i][0] != '-')
             break;
-        switch(argv[i][1]){
-            case 'h':
-                exit_with_help();
+        switch(argv[i][1]) {
+            case 'h': // help
+                show_help();
+                return false;
+            case 's': // single mode flag
+                mode = Single;
                 break;
-            case 's':
-                single_mode = true;
+            case 'g': // group mode
+                mode = Group;
                 break;
-            case 'p':
-                if (single_mode){
-                    int r = (double)atoi(argv[++i]);
-                    int phi = atoi(argv[++i]);
-                    int theta = atoi(argv[++i]);
-                    position =from_polar((double)r,(double)phi,(double)theta);
-                }else if (group_mode){
-                    camera_distance = (double)atoi(argv[++i]);
-                    position = from_polar(camera_distance,0,0);
-                }else{
-                    exit_with_help();
+            case 'p': // position
+                switch (mode) {
+                    case Single:
+                        camera_distance = (double)atoi(argv[++i]);
+                        phi = (double)atoi(argv[++i]);
+                        theta = (double)atoi(argv[++i]);
+                        position = from_polar(camera_distance, phi, theta);
+                        break;
+                    case Group:
+                        camera_distance = (double)atoi(argv[++i]);
+                        position = from_polar(camera_distance, 0, 0);
+                        break;
+                    default:
+                        show_help();
+                        return false;
                 }
                 break;
-            case 'c':
-                background_color[0] = (double)atoi(argv[++i]);
-                background_color[0] = (double)atoi(argv[++i]);
-                background_color[0] = (double)atoi(argv[++i]);
+            case 'c': // color
+                background[0] = (double)atoi(argv[++i]) / 255.0;
+                background[1] = (double)atoi(argv[++i]) / 255.0;
+                background[2] = (double)atoi(argv[++i]) / 255.0;
                 break;
-            case 'n':
-                number_of_images = atoi(argv[++i]);
+            case 'n': // square root of number of images
+                step_count = atoi(argv[++i]);
                 break;
-            case 'f':
-                model_file_name = argv[++i];
+            case 'f': // input model file
+                input = argv[++i];
                 break;
-            case 't':
-                output_file_name = argv[++i];
-                break;
-            case 'g':
-                group_mode = true;
+            case 't': // output image file
+                output = argv[++i];
                 break;
         }
         i++;
     }
-    if (!single_mode && !group_mode)
-        exit_with_help();
-    if (model_file_name == NULL || output_file_name == NULL)
-        exit_with_help();
+    if (input.length() == 0 || output.length() == 0) { // invalid file name
+        show_help();
+        return false;
+    }
+    return true;
 }
 
 int main(int argc, char **argv) {
 
-    double min_angle = 0;
-    double max_angle = 360;
+    background = new double[3];
+    background[0] = 0;
+    background[1] = 0;
+    background[2] = 0;
 
-    background_color = new double[3];
-    background_color[0] = 0;
-    background_color[1] = 0;
-    background_color[2] = 0;
+    position = from_polar(camera_distance, 0, 0); // initial position
 
-    position = from_polar(camera_distance,0,0);
+    if (!parse_command_line(argc, argv))
+        return EXIT_FAILURE;
 
-    parse_command_line(argc,argv);
-
-    //read the model
-    vtkSmartPointer<vtkPLYReader> reader =
-            vtkSmartPointer<vtkPLYReader>::New();
-    reader->SetFileName (model_file_name);
+    // Read the model
+    vtkSmartPointer<vtkPLYReader> reader = vtkSmartPointer<vtkPLYReader>::New();
+    reader->SetFileName(input.c_str());
 
     // Visualize
-    vtkSmartPointer<vtkPolyDataMapper> mapper =
-            vtkSmartPointer<vtkPolyDataMapper>::New();
+    vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
     mapper->SetInputConnection(reader->GetOutputPort());
 
-    vtkSmartPointer<vtkActor> actor =
-            vtkSmartPointer<vtkActor>::New();
+    vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
     actor->SetMapper(mapper);
 
     double* bound = mapper->GetBounds();
     Cartesian center((bound[1] - bound[0]) / 2, (bound[3] - bound[2]) / 2, (bound[5] - bound[4]) / 2);
 
-    string output(output_file_name);
-    string input(model_file_name);
+    // common prefix
+    if (mode == Group)
+        output = output + input.substr(input.find_last_of('/') + 1, input.find_last_of('.') - input.find_last_of('/') - 1);
 
-    if (group_mode)
-        output = output + input.substr(input.find_last_of('/')+1,input.find_last_of('.') - input.find_last_of('/') - 1);
+    // compute delta angle of each iteration
+    double step = (max_angle - min_angle) / step_count;
 
-    double step = (max_angle - min_angle)/number_of_images,theta = 0.0, phi = 0.0;
+    // use all possible angle combinations
+    for(int i = 0; i < step_count; i++, theta += step) {
+        for(int j = 0; j < step_count; j++, phi += step) {
 
-    for(int i = 0; i < number_of_images; i++) {
-        for(int j = 0; j < number_of_images;j++) {
-            vtkSmartPointer<vtkCamera> camera =
-                    vtkSmartPointer<vtkCamera>::New();
+            position = from_polar(camera_distance, phi,theta);
+
+            vtkSmartPointer<vtkCamera> camera = vtkSmartPointer<vtkCamera>::New();
             camera->SetPosition(position.x + center.x, position.y + center.y, position.z + center.z);
             camera->SetFocalPoint(center.x, center.y, center.z);
 
-            vtkSmartPointer<vtkRenderer> renderer =
-                    vtkSmartPointer<vtkRenderer>::New();
+            vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
             renderer->SetActiveCamera(camera);
             renderer->AddActor(actor);
-            renderer->SetBackground(background_color); // Black background
+            renderer->SetBackground(background); // Black background
 
-            vtkSmartPointer<vtkRenderWindow> renderWindow =
-                    vtkSmartPointer<vtkRenderWindow>::New();
+            vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
             renderWindow->AddRenderer(renderer);
 
             renderWindow->Render();
 
-            vtkSmartPointer<vtkWindowToImageFilter> windowToImageFilter =
-                    vtkSmartPointer<vtkWindowToImageFilter>::New();
+            vtkSmartPointer<vtkWindowToImageFilter> windowToImageFilter = vtkSmartPointer<vtkWindowToImageFilter>::New();
             windowToImageFilter->SetInput(renderWindow);
-            windowToImageFilter->SetMagnification(
-                    3); //set the resolution of the output image (3 times the current resolution of vtk render window)
+            windowToImageFilter->SetMagnification(2); //set the resolution of the output image
             windowToImageFilter->SetInputBufferTypeToRGB();
             windowToImageFilter->ReadFrontBufferOff(); // read from the back buffer
             windowToImageFilter->Update();
@@ -207,19 +222,19 @@ int main(int argc, char **argv) {
             vtkSmartPointer<vtkPNGWriter> writer =
                     vtkSmartPointer<vtkPNGWriter>::New();
 
-
+            // use angles in the file name
             string suffix = "";
-            if (group_mode)
-                suffix = '_'+to_string((int)theta) + '_' + to_string((int)phi)+".png";
+            if (mode == Group)
+                suffix = '_' + to_string((int)theta) + '_' + to_string((int)phi) + ".png";
 
-            writer->SetFileName((output+suffix).c_str());
+            writer->SetFileName((output + suffix).c_str());
             writer->SetInputConnection(windowToImageFilter->GetOutputPort());
             writer->Write();
 
-            theta = step * (i+1);
-            phi = step * (j+1);
-            position = from_polar(camera_distance, phi,theta);
         }
     }
+
+    delete[] background;
+
     return EXIT_SUCCESS;
 }
