2af374992db9047b36b578ecb4702bbc61b778d8
lyx-x <li.yuxiang.nj@gmail.com>
2015-11-14 20:46:55 +0100

    Add K-Mean program specification

--- utils/KMean/k_mean.cpp
+++ utils/KMean/k_mean.cpp
@@ -1,3 +1,23 @@
+/*
+ * K-mean clustering
+ *
+ * This program implements the K-Mean clustering method. It takes a set of points (lines in a binary file) and
+ * returns the group number of each point and an extra file to describe the group, namely the group center.
+ *
+ * Usage:
+ *      k_mean [Path_to_file]
+ *
+ * N.B. The file format is very specific, it is a binary file with integers and floats, so please pay attention to the
+ * big / little endian problem. You may want to generate the file by program in case of theses sorts of problems.
+ * The first number is the number of points: N, the second is the dimension of the point: d. Then there should
+ * be N * d float numbers after. So the binary file looks like:
+ *
+ *      N (32 bits integer) d (32 bits integer)
+ *      P_1 (d * 32 bits float)
+ *      ...
+ *      P_N (d * 32 bits float)
+ */
+
 #include <iostream>
 
 using namespace std;
