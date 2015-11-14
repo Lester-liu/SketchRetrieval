c42a6cc1ce44d7f68a51bbfb5f9cfded7a08cd68
nsfxc <nnssffxxcc@gmail.com>
2015-11-14 21:57:01 +0100

    contour file generation

---
+++ utils/contour_extraction.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# One folders with PNG format files and one folder to the contour
+
+Files="$1"
+Dest_Folder="$2"
+
+
+for Folder in $Files*/
+do
+    Dest_Folder_Name="${Folder%/}"
+    Dest_Folder_Name="${Dest_Folder_Name##*/}"
+    # Create the folder
+    rm -rf "$Dest_Folder$Dest_Folder_Name/";
+    mkdir "$Dest_Folder$Dest_Folder_Name/";
+
+    for Src in $Folder*
+    do
+            # Get the file name (after the last '/')
+            File="${Src##*/}"
+            # Run the contour_extraction
+            ./Contour/Debug/contour $Src $Dest_Folder$Dest_Folder_Name/$File;
+            #echo $Dest_Folder$Dest_Folder_Name/
+            #echo $Dest_Folder$Dest_Folder_Name/$File
+    done
+done
