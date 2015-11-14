17bd25bf7d667d51a652dba1688f1f2be320f1e1
lyx-x <li.yuxiang.nj@gmail.com>
2015-11-14 20:59:07 +0100

    Add Canny specifications

--- utils/Contour/canny.cpp
+++ utils/Contour/canny.cpp
@@ -1,3 +1,16 @@
+/*
+ * Contour extraction with Canny algorithm
+ *
+ * This program uses Canny module from OpenCV to extract the contour of an image. For the algorithm's detail,
+ * please read the OpenCV documentation.
+ *
+ * Parameters:
+ *      f (path to the input image)
+ *      t (path to store the result)
+ *      s (parameters to tune the result)
+ * Ex: contour -f [Path_to_input_image] -t [Path_to_contour_image] [-s max_threshold min_threshold]
+ */
+
 #include <iostream>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
