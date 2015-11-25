0758e471802b388dacd1b448cc6ac26f907306fc
nsfxc <nnssffxxcc@gmail.com>
2015-11-25 21:01:16 +0100

    encoder shell modification

--- utils/encoder.sh
+++ utils/encoder.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+#exemple: encoder.sh ../Sketch/pipeline/bin/ /Sketch/pipeline/dict.txt
 
-Path="$1"
-Dict="$2"
-Name="$3"
-Output="$4"
-Cases="$5"
+Path="$1" #path of input(ex: binary folder)
+Dict="$2" #path of dictionary
+Output="$3" #path of output folder
+Cases="$4" #case number
 
 for Folder in $Path*/
 do
-    ./KMean/Debug/./k_mean -f $Folder -d $Dict -s 32 -c $Cases -a 1089 -o $Output
+    Dest_Name="${Folder%/}"
+    Dest_Name="${Dest_Folder_Name##*/}"
+    ./KMean/Debug/./k_mean -f $Folder -d $Dict -s 32 -c $Cases -a 1089 -o $Output$Dest_Name.transf
 done
