e8275c8345ffcf2be06e205132bc2bd9807c1052
lyx-x <li.yuxiang.nj@gmail.com>
2015-11-26 10:50:30 +0100

    Add comments

--- utils/database.sh
+++ utils/database.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+
+# Ex:
+
 Input="$1" # input folder with encoded files
-Output_data="$2" # output file
-Output_index="$3"
+Output_data="$2" # output tf-idf file
+Output_index="$3" # output label file
 K="$4" # number of words
 
 ./Database/Debug/./build_database -i $Input -d $Output_data -m $Output_index -k $K
