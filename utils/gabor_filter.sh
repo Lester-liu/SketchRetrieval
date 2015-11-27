db6032f671d234a7d71f010a0494d30ec0ebbf11
nsfxc <nnssffxxcc@gmail.com>
2015-11-27 18:56:21 +0100

    Debug

--- utils/gabor_filter.sh
+++ utils/gabor_filter.sh
@@ -7,12 +7,13 @@ View_Foler="$1"
 Contour_Folder="$2"
 Dest_Folder="$3"
 Name="$4"
+Picture_Number="$5"
 
 for Folder in $View_Foler*/
 do
     Dest_Name="${Folder%/}"
     Dest_Name="${Dest_Name##*/}"
     #echo "./Gabor/Debug/./gabor -i $Folder$Name -o $Dest_Folder$Dest_Name.bin -a $Contour_Folder$Dest_Name/"
-    ./Gabor/Debug/./gabor -i $Folder$Name -o $Dest_Folder$Dest_Name.bin -a $Contour_Folder$Dest_Name/
+    ./Gabor/Debug/./gabor -i $Folder$Name -o $Dest_Folder$Dest_Name.bin -a $Contour_Folder$Dest_Name/ -m $Picture_Number
 
 done
